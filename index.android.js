29d3757ff2af9b173f882a7663dd40c7063f5d21
twillzy <william.tanzy@gmail.com>
2016-11-27 03:37:46 +1100

    socket on client

--- index.android.js
+++ index.android.js
@@ -5,20 +5,33 @@ import {
   Text,
   View
 } from 'react-native';
-
-import BLEControllerModule from './App/Modules/BLEControllerModule';
+window.navigator.userAgent = 'ReactNative';
+const io = require('socket.io-client/socket.io');
 
 class HelloWorld extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      budget : 200
+      budget : 200,
+      spent: 0,
+      isPoor: false,
+      drinks: []
     };
+    const socket = io('https://fathomless-peak-84606.herokuapp.com/', {
+      transports: ['websocket']
+    });
+    socket.on('drinkBought', (data) => {
+      this.setState({spent: data.totalPrice});
+    });
+    socket.on('')
+
   }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.start}>Budget: ${this.state.budget}</Text>
+        <Text style={styles.start}>Budget: ${this.state.spent}</Text>
       </View>
     )
   }
